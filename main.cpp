8f82737a5c171270256abb07d42a652bc31bf2f7
christina05011 <42581109+christina05011@users.noreply.github.com>
2022-01-31 23:05:20 -0500

    Main Scanner+Parser

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include "scanner.h"
+#include "parser.h"
 #include <iostream>
 
 using namespace std;
 
 int main() {
+	cout << "------------------------------SCANNER------------------------------" << endl;
 	get("../programa.txt");
+	get_vector(tokens);
+	cout << "------------------------------PARSER------------------------------" << endl;
+	parser();	
 	return 0;
 }
